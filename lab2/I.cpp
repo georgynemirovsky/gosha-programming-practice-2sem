232a64be86e70ab6f8b5e832a265f11264d6333a
gosha <nemirovsky.georgyi@gmail.com>
2022-02-10 22:25:27 +0300

    new I

--- lab2/I.cpp
+++ lab2/I.cpp
@@ -10,9 +10,9 @@ int main()
     while (a > 1 and b <= a){
         if (a % b == 0){
             while (a % b == 0){
-                cout << b << " ";
                 a = a / b;
             }
+            cout << b << " ";
         }
         b++;
     }
