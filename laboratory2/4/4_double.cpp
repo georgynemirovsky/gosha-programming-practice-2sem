4da960c9c790a2f24d32c7754726e72df0c1d20a
gosha <nemirovsky.georgyi@gmail.com>
2022-03-17 02:55:48 +0300

    4

---
+++ laboratory2/4/4_double.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double FMA(double psi[], double pdf[], double const dv, unsigned size)
+{
+    double sum = 0.0;
+    for (int i = 0; i < size; i++)
+    {
+        sum = fma(psi[i], pdf[i], sum);
+    }
+    return fma(sum, dv, 0);
+}
+int main()
+{
+    double T = 0.f;
+    cin >> T;
+    long long int n = 1000;
+    double PI = acos(-1.0);
+    double x = 0.f, dv = 0.f;
+    if (T >= 1)
+    {
+        x = -T;
+        dv = 2 * T / n;
+    } else
+    {
+        x = -1 / sqrt(T);
+        dv = 2 / sqrt(T) / n;
+    }
+    double *pdf = 0;
+    double *psi = 0;
+    psi = new double[n];
+    pdf = new double[n];
+    for (long long int i = 0; i < n; i++)
+    {
+        x = x + dv;
+        psi[i] = abs(x);
+        pdf[i] =exp(- x * x / T) / sqrt(PI * T);
+    }
+    cout << FMA(psi, pdf, dv, n) << endl;
+    delete [] pdf;
+    return 0;
+}
---
+++ laboratory2/4/4_float.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float FMA(float psi[], float pdf[], float const dv, unsigned size)
+{
+    float sum = 0.0;
+    for (int i = 0; i < size; i++)
+    {
+        sum = fma(psi[i], pdf[i], sum);
+    }
+    return fma(sum, dv, 0);
+}
+int main()
+{
+    float T = 0.f;
+    cin >> T;
+    long long int n = 1000;
+    float PI = acos(-1.0);
+    float x = 0.f, dv = 0.f;
+    if (T >= 1)
+    {
+        x = -T;
+        dv = 2 * T / n;
+    } else
+    {
+        x = -1 / sqrt(T);
+        dv = 2 / sqrt(T) / n;
+    }
+    float *pdf = 0;
+    float *psi = 0;
+    psi = new float[n];
+    pdf = new float[n];
+    for (long long int i = 0; i < n; i++)
+    {
+        x = x + dv;
+        psi[i] = abs(x);
+        pdf[i] =exp(- x * x / T) / sqrt(PI * T);
+    }
+    cout << FMA(psi, pdf, dv, n) << endl;
+    delete [] pdf;
+    return 0;
+}
