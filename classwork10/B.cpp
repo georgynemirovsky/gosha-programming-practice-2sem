999d39acd92018eada4c5e399af17ef744bae84c
gosha <nemirovsky.georgyi@gmail.com>
2022-04-22 23:58:54 +0300

    C

--- classwork10/B.cpp
+++ classwork10/B.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+int Fib(int n) {
+    if (n == 1 or n == 2) {
+        return 1;
+    }
+    if (n == 3) {
+        return 2;
+    }
+    return Fib(n - 1) + Fib(n - 2) + Fib(n - 3);
+}
+
+
 int main() {
+    int n = 0;
+    cin >> n;
+    cout << Fib(n + 1) << endl;
     return 0;
 }
--- classwork10/C.cpp
+++ classwork10/C.cpp
@@ -2,6 +2,44 @@
 
 using namespace std;
 
+struct integer {
+    int number;
+    int counts;
+
+    integer () {
+        number = 0;
+        counts = 1;
+    }
+};
+
+
+int th (integer* mass, int n) {
+    int i = 0;
+    int j = 1;
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < j; i++) {
+            if (mass[i].number < mass[j].number and mass[j].counts <= mass[i].counts) {
+                mass[j].counts++;
+            }
+        }
+    }
+    int m = 0;
+    for (int i = 0; i < n; i++) {
+        if (m < mass[i].counts) {
+            m = mass[i].counts;
+        }
+    }
+    return m;
+}
 int main() {
+    int n = 0;
+    cin >> n;
+    integer *mass = new integer [n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> mass[i].number;
+    }
+
+    cout << th(mass, n) << endl;
     return 0;
 }
