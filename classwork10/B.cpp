6b44e67fc4313bd6e8bd25193e50837c20e68f57
gosha <nemirovsky.georgyi@gmail.com>
2022-04-23 13:43:39 +0300

    B

--- classwork10/B.cpp
+++ classwork10/B.cpp
@@ -2,20 +2,40 @@
 
 using namespace std;
 
-int Fib(int n) {
-    if (n == 1 or n == 2) {
-        return 1;
+struct Element {
+    int number = 0;
+    int counts = 1;
+
+    element () {
+        number = 0;
+        counts = 1;
     }
-    if (n == 3) {
-        return 2;
+};
+
+int minimal(int a, int b, int c) {
+    if (a > b) {
+        a = b;
     }
-    return Fib(n - 1) + Fib(n - 2) + Fib(n - 3);
+    if (a > c) {
+        a = c;
+    }
+    return a;
 }
-
-
+int price (Element *steps, int n) {
+    for (int i = 3; i < n; i++) {
+        steps[i].counts += minimal(steps[i - 1].counts, steps[i - 2].counts, steps[i - 3].counts);
+    }
+    return steps[n - 1].counts;
+}
 int main() {
     int n = 0;
     cin >> n;
-    cout << Fib(n + 1) << endl;
+    Element *steps = new Element [n];
+    for (int i = 0; i < n; i++) {
+        steps[i].number = i + 1;
+        cin >> steps[i].counts;
+    }
+
+    cout << price(steps, n) << endl;
     return 0;
 }
