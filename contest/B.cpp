19b5d26edf1861411bdfa837947e2493d1b9d151
gosha <nemirovsky.georgyi@gmail.com>
2022-05-05 22:02:09 +0300

    contest

--- contest/B.cpp
+++ contest/B.cpp
@@ -25,12 +25,19 @@ int part(vector<int> &A, int lo, int hi) {
     }
 }
 
-void quicksort(vector<int> &A, int lo, int hi)
-{
+void quicksort(vector<int> &A, int lo, int hi) {
     if (lo < hi) {
         int p = part(A, lo, hi);
         quicksort(A, lo, p);
         quicksort(A, p + 1, hi);
+    }
+}
+
+int minimal(int a, int b) {
+    if (a > b) {
+        return b;
+    } else {
+        return a;
     }
 }
 
@@ -39,39 +46,23 @@ int main()
     int number = 0;
     int s = 0;
     vector<int> arr;
+    vector<int> length;
     while (cin >> number) {
         arr.push_back(number);
     }
+
     quicksort(arr, 0, arr.size() - 1);
-    int *it = new int[arr.size()];
 
-    for (int i = 0; i < arr.size(); i++) {
-        it[i] = 0;
-    }
-
-    it[0] = 1;
-    it[1] = 1;
-    it[arr.size() - 1] = 1;
-    it[arr.size() - 2] = 1;
-
-    for (int i = 0; i < arr.size() - 2; i++) {
-        if (it[i] == 0) {
-            if (arr[i] - arr[i - 1] < arr[i + 1] - arr[i]) {
-                it[i] = 1;
-                s += arr[i] - arr[i - 1];
-            } else {
-                it[i + 1] = 1;
-                it[i] = 1;
-                s+= arr[i + 1] - arr[i];
-            }
-        }
-    }
-    if (arr.size() >= 4) {
-        cout << arr[1] - arr[0] + s + arr[arr.size() - 1] - arr[arr.size() - 2] << endl;
-    } else if (arr.size() == 2) {
+    if (arr.size() == 2) {
         cout << arr[1] - arr[0] << endl;
-    } else if (arr.size() == 3) {
-        cout << arr[2] - arr[0] << endl;
+        return 0;
     }
+    length.push_back(arr[1] - arr[0]);
+    length.push_back(arr[2] - arr[0]);
+    for (int i = 2; i < arr.size() - 1; i++) {
+        length.push_back(minimal(length[i - 1], length[i - 2]) + arr[i + 1] - arr[i]);
+    }
+
+    cout << length[length.size() - 1] << endl;
     return 0;
 }
